86715d5a6391ca548fe49a46d48a720a3f9832d4
Geoffrey <geoffreyraditya12@gmail.com>
2025-11-10 22:11:36 +0700

    Commit No. 3 – UI Element and Theme

--- app/src/main/java/com/example/lab_week_9/ui/theme/Color.kt
+++ app/src/main/java/com/example/lab_week_9/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.lab_week_9.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme Colors
+val Purple80 = Color(0xFFA0CAFF) // Light Blue
+val PurpleGrey80 = Color(0xFFC9C5D0) // Light Neutral Gray
+val Pink80 = Color(0xFF93F0DD)     // Light Teal
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme Colors
+val Purple40 = Color(0xFF004A77) // Dark Blue
+val PurpleGrey40 = Color(0xFF49454E) // Dark Neutral Gray
+val Pink40 = Color(0xFF005047)     // Dark Teal
